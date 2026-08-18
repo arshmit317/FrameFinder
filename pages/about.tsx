d4afbe137af369674e73d222cc63b10d6d9b5eda
Sarah <sarah.alareebi106@gmail.com>
2026-08-18 15:17:46 -0400

    Update About page

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,10 +2,10 @@ export default function About() {
   return (
     <div className="pageContainer">
       <main>
-        <h2>About Film Finder</h2>
+        <h2>About FrameFinder</h2>
 
         <p>
-          We are a group of five people who share a love for movies and enjoy discovering new films together. We wanted to create a simple and reliable website that makes finding the right movie easier. 
+          We are a group of four people who share a love for movies and enjoy discovering new films together. We wanted to create a simple and reliable website that makes finding the right movie easier. 
           That idea led us to build FrameFinder, a movie search and rating website where users can explore different movies, search by title or genre, view ratings, and save movies they may want to watch later. 
           Our goal is to help people spend less time searching and more time enjoying great movies.
         </p>
